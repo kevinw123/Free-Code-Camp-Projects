c6f960ae08426fab83db380b756997eadedd0c6b
kevinw123 <kevinwong_895@hotmail.com>
2016-07-18 14:12:42 -0700

    Adding image slider background

--- RandomQuote/js/app.js
+++ RandomQuote/js/app.js
@@ -1,1 +1,19 @@
-app.js
+$(document).ready(function(){
+var header = $('body');
+
+var backgrounds = new Array(
+    'url(img/mountain1.jpg)'
+  , 'url(img/mountain2.jpg)'
+);
+
+var current = 0;
+
+function nextBackground() {
+    current++;
+    current = current % backgrounds.length;
+    header.css('background-image', backgrounds[current]);
+}
+setInterval(nextBackground, 2000);
+
+header.css('background-image', backgrounds[0]);
+});

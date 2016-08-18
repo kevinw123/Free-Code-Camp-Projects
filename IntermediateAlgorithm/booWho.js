e70cf09f19b3c4075c1de120e1f0c6a731fb85f9
Kevin Wong <kevinwong_895@hotmail.com>
2016-08-18 16:31:13 -0700

    booWho

---
+++ IntermediateAlgorithm/booWho.js
@@ -0,0 +1,11 @@
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  var result = bool;
+  if(result === true || result === false){
+    return true;
+  }
+  else
+    return false;
+}
+
+booWho("a");

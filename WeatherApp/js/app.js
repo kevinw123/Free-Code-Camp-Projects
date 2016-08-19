92491cb654de6d428d5e734c989dfaa412ec79ac
Kevin Wong <kevinwong_895@hotmail.com>
2016-08-19 10:14:19 -0700

    Getting correct city

--- WeatherApp/js/app.js
+++ WeatherApp/js/app.js
@@ -1,8 +1,10 @@
 var location;
 $(document).ready(function(){
   $.getJSON("http://ip-api.com/json", function(data){
-      location.latitude = Math.floor(data.lat);
-      location.longitude = Math.floor(data.lon);
+      location.latitude = getLatitude(data); //Math.floor(getLatitude(data)); 
+      location.longitude = getLongitude(data);//Math.floor(getLongitude(data));
+      /*var myData = JSON.parse(city.list);
+      console.log(myData);*/
       var webAddress = "http://api.openweathermap.org/data/2.5/weather?lat="+location.latitude +"&lon=" + location.longitude + "&APPID=b20df3281dd211ad4c1254577573e180";
         $.getJSON(webAddress, function(data){
           console.log(data);
@@ -11,4 +13,10 @@ $(document).ready(function(){
       console.log(location.longitude);
   });
 
+    function getLongitude(data){
+      return data.lon;
+    }
+    function getLatitude(data){
+      return data.lat;
+    }
 });

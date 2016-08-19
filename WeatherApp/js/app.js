10b40c458b7dcfe0b2a1155866c51d3b4208b4d8
kevinw123 <kevinwong_895@hotmail.com>
2016-08-18 23:21:40 -0700

    getting longitude and latitude from current location

--- WeatherApp/js/app.js
+++ WeatherApp/js/app.js
@@ -1,25 +1,14 @@
 var location;
 $(document).ready(function(){
   $.getJSON("http://ip-api.com/json", function(data){
-      location.latitude = data.lat;
-      location.longitude = data.lon;
-
+      location.latitude = Math.floor(data.lat);
+      location.longitude = Math.floor(data.lon);
+      var webAddress = "http://api.openweathermap.org/data/2.5/weather?lat="+location.latitude +"&lon=" + location.longitude + "&APPID=b20df3281dd211ad4c1254577573e180";
+        $.getJSON(webAddress, function(data){
+          console.log(data);
+        });
       console.log(location.latitude);
       console.log(location.longitude);
   });
-
-var webAddress = "http://api.openweathermap.org/data/2.5/weather?lat=49&lon=-123&APPID=b20df3281dd211ad4c1254577573e180";
-
-  $.ajax({
-    url: webAddress,
-    dataType: "jsonp",
-    success: function(res) {
-        console.log(res.name);
-    },
-    error: function(xhr, status, error) {
-         console.log("Error");
-  }
-  });
-
 
 });

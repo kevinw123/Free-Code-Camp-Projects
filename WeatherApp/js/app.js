676766d7a9bee44343a2f371a39a671d5d6b4210
kevinw123 <kevinwong_895@hotmail.com>
2016-08-18 22:58:13 -0700

    Getting json data for longitude, latitude and openweatherdata

--- WeatherApp/js/app.js
+++ WeatherApp/js/app.js
@@ -1,19 +1,25 @@
-  var output = document.getElementById("out");
+var location;
+$(document).ready(function(){
+  $.getJSON("http://ip-api.com/json", function(data){
+      location.latitude = data.lat;
+      location.longitude = data.lon;
 
-  if (!navigator.geolocation){
-    console.log("Geolocation is not supported by your browser");
+      console.log(location.latitude);
+      console.log(location.longitude);
+  });
 
+var webAddress = "http://api.openweathermap.org/data/2.5/weather?lat=49&lon=-123&APPID=b20df3281dd211ad4c1254577573e180";
+
+  $.ajax({
+    url: webAddress,
+    dataType: "jsonp",
+    success: function(res) {
+        console.log(res.name);
+    },
+    error: function(xhr, status, error) {
+         console.log("Error");
   }
+  });
 
-  function success(position) {
-    var latitude  = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    console.log("latitude " + latitude);
-    console.log("longitude " + longitude);
-  };
 
-  function error() {
-    console.log("error");
-  };
-
-  navigator.geolocation.getCurrentPosition(success, error);
+});

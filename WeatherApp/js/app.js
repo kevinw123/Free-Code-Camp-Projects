46a5d84a1c476c031067a7ca4efa0d83947c0681
kevinw123 <kevinwong_895@hotmail.com>
2016-08-19 19:21:40 -0700

    modifying UI

--- WeatherApp/js/app.js
+++ WeatherApp/js/app.js
@@ -1,39 +1,38 @@
 var location;
-$(document).ready(function(){
-  $.getJSON("http://ip-api.com/json", function(data){
-      location.latitude = getLatitude(data);
-      location.longitude = getLongitude(data);
-      location.regionName = getRegionName(data);
-      location.city = getCity(data);
-      var webAddress = "http://api.openweathermap.org/data/2.5/weather?lat="+location.latitude +"&lon=" + location.longitude + "&APPID=b20df3281dd211ad4c1254577573e180";
-        $.getJSON(webAddress, function(data){
-          console.log(data);
-          console.log(data.weather[0].description);
-          console.log(data.weather[0].main);
-          console.log(data.name);
-          console.log(data.main.temp);
-          $("#message").text(data.weather[0].description);
+$(document).ready(function() {
+    $.getJSON("http://ip-api.com/json", function(data) {
+        location.latitude = getLatitude(data);
+        location.longitude = getLongitude(data);
+        location.regionName = getRegionName(data);
+        location.city = getCity(data);
+        var webAddress = "http://api.openweathermap.org/data/2.5/weather?lat=" + location.latitude + "&lon=" + location.longitude + "&APPID=b20df3281dd211ad4c1254577573e180";
+        $.getJSON(webAddress, function(data) {
+            console.log(data);
+            var icon = data.weather[0].icon.replace(/"/g,"");
+            $("#weather").text(data.weather[0].main);
+            $("#description").text(data.weather[0].description);
+            $("#temperature").text(data.main.temp + " F");
+            console.log(icon);
+            //$("icon").attr("src","http://openweathermap.org/img/w/"+icon+".png");
         });
-      $("#city").text(location.city + ", " + location.regionName);
-  });
 
-    function getLongitude(data){
-      return data.lon;
+        $("#city").text(location.city + ", " + location.regionName);
+    });
+
+    function getLongitude(data) {
+        return data.lon;
     }
-    function getLatitude(data){
-      return data.lat;
+
+    function getLatitude(data) {
+        return data.lat;
     }
-    function getRegionName(data){
-      return data.regionName;
+
+    function getRegionName(data) {
+        return data.regionName;
     }
-    function getCity(data){
-      return data.city;
+
+    function getCity(data) {
+        return data.city;
     }
 
 });
-
-/*$(function() {
-  h = $(window).height();
-  w = $(window).width();
-  $(html).style('background','url(..img/sunny.jpg&w='+w+'&h='+h);
-});*/

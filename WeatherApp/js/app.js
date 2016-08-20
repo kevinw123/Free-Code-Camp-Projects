1a259eb05abc4133279f6691e47c00f67c62b9e1
kevinw123 <kevinwong_895@hotmail.com>
2016-08-20 11:52:39 -0700

    Getting all four boxes to display data

--- WeatherApp/js/app.js
+++ WeatherApp/js/app.js
@@ -1,7 +1,8 @@
 var location;
-location.deg = "F";
-location.tempC = 0.0;
-location.tempF = 0.0;
+location.deg = ["F", "F", "F", "F"];
+location.tempC = [0.0, 0.0, 0.0, 0.0];
+location.tempF = [0.0, 0.0, 0.0, 0.0];
+
 $(document).ready(function() {
     $.getJSON("http://ip-api.com/json", function(data) {
         location.latitude = getLatitude(data);
@@ -13,11 +14,13 @@ $(document).ready(function() {
         var webAddress = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=" + location.latitude + "&lon=" + location.longitude + "&cnt=4&APPID=b20df3281dd211ad4c1254577573e180";
         $.getJSON(webAddress, function(data) {
             console.log(data);
-            location.tempF = data.list[0].deg;
-            $("#weather").text(data.list[0].weather[0].main);
-            $("#temperature").text(Math.floor(location.tempF));
-            $("#degree").text('\u00B0' + location.deg);
-            $("#icon").attr("src", getIcon(data));
+            for (var i = 0; i < 4; i++) {
+                location.tempF[i] = data.list[i].deg;
+                $("#weather" + i).text(data.list[i].weather[0].main);
+                $("#temperature" + i).text(Math.floor(location.tempF[i]));
+                $("#degree" + i).text('\u00B0' + location.deg[i]);
+                $("#icon" + i).attr("src", getIcon(data, i));
+            }
         });;
     });
 
@@ -37,49 +40,91 @@ $(document).ready(function() {
         return data.city;
     }
 
-    function getIcon(data) {
+    function getIcon(data, i) {
         var path = "";
-        if (200 < data.list[0].weather[0].id && data.list[0].weather[0].id < 300) {
+
+        if (200 < data.list[i].weather[0].id && data.list[i].weather[0].id < 300) {
             path = "img/thunder.png";
             return path;
-        } else if (300 <= data.list[0].weather[0].id && data.list[0].weather[0].id < 400) {
+        } else if (300 <= data.list[i].weather[0].id && data.list[i].weather[0].id < 400) {
             path = "img/drizzle.png";
             return path;
-        } else if (500 <= data.list[0].weather[0].id && data.list[0].weather[0].id < 600) {
+        } else if (500 <= data.list[i].weather[0].id && data.list[i].weather[0].id < 600) {
             path = "img/rain.png";
             return path;
-        } else if (600 <= data.list[0].weather[0].id && data.list[0].weather[0].id < 700) {
+        } else if (600 <= data.list[i].weather[0].id && data.list[i].weather[0].id < 700) {
             path = "img/snow.png";
             return path;
-        } else if (700 <= data.list[0].weather[0].id && data.list[0].weather[0].id < 800) {
+        } else if (700 <= data.list[i].weather[0].id && data.list[i].weather[0].id < 800) {
             path = "img/atmosphere.png";
             return path;
-        } else if (data.list[0].weather[0].id == 800) {
+        } else if (data.list[i].weather[0].id == 800) {
             path = "img/clear.png";
             return path;
-        } else if (801 <= data.list[0].weather[0].id && data.list[0].weather[0].id <= 804) {
+        } else if (801 <= data.list[i].weather[0].id && data.list[i].weather[0].id <= 804) {
             path = "img/cloud.png";
             return path;
-        } else if (900 <= data.list[0].weather[0].id && data.list[0].weather[0].id <= 906) {
+        } else if (900 <= data.list[i].weather[0].id && data.list[i].weather[0].id <= 906) {
             path = "img/extreme.png";
             return path;
-        } else if (951 <= data.list[0].weather[0].id && data.list[0].weather[0].id <= 962) {
+        } else if (951 <= data.list[i].weather[0].id && data.list[i].weather[0].id <= 962) {
             path = "img/additional.png";
             return path;
         }
+
     }
 
-    $("#degree").click(function() {
-        console.log("Handler for .click() called.");
-
-        if (location.deg == "C") {
-            location.deg = "F";
-            location.tempC = Math.floor((location.tempF -32) * (5/9));
-            $("#temperature").text(location.tempC);
-        } else {
-            location.deg = "C";
-            $("#temperature").text(location.tempF);
+    $("#degree0").click(function() {
+        for (var i = 0; i < 4; i++) {
+            if (location.deg[i] == "C") {
+                location.deg[i] = "F";
+                location.tempC[i] = Math.floor((location.tempF[i] - 32) * (5 / 9));
+                $("#temperature" + i).text(location.tempC[i]);
+            } else {
+                location.deg[i] = "C";
+                $("#temperature" + i).text(location.tempF[i]);
+            }
+            $("#degree" + i).text('\u00B0' + location.deg[i]);
         }
-        $("#degree").text('\u00B0' + location.deg);
+    });
+    $("#degree1").click(function() {
+        for (var i = 0; i < 4; i++) {
+            if (location.deg[i] == "C") {
+                location.deg[i] = "F";
+                location.tempC[i] = Math.floor((location.tempF[i] - 32) * (5 / 9));
+                $("#temperature" + i).text(location.tempC[i]);
+            } else {
+                location.deg[i] = "C";
+                $("#temperature" + i).text(location.tempF[i]);
+            }
+            $("#degree" + i).text('\u00B0' + location.deg[i]);
+        }
+    });
+
+    $("#degree2").click(function() {
+        for (var i = 0; i < 4; i++) {
+            if (location.deg[i] == "C") {
+                location.deg[i] = "F";
+                location.tempC[i] = Math.floor((location.tempF[i] - 32) * (5 / 9));
+                $("#temperature" + i).text(location.tempC[i]);
+            } else {
+                location.deg[i] = "C";
+                $("#temperature" + i).text(location.tempF[i]);
+            }
+            $("#degree" + i).text('\u00B0' + location.deg[i]);
+        }
+    });
+    $("#degree3").click(function() {
+        for (var i = 0; i < 4; i++) {
+            if (location.deg[i] == "C") {
+                location.deg[i] = "F";
+                location.tempC[i] = Math.floor((location.tempF[i] - 32) * (5 / 9));
+                $("#temperature" + i).text(location.tempC[i]);
+            } else {
+                location.deg[i] = "C";
+                $("#temperature" + i).text(location.tempF[i]);
+            }
+            $("#degree" + i).text('\u00B0' + location.deg[i]);
+        }
     });
 });

fb2bbce853167a369ebbe7d0fb887b1b9abafe6b
kevinw123 <kevinwong_895@hotmail.com>
2016-08-19 23:48:04 -0700

    Fixing UI and js code

--- WeatherApp/js/app.js
+++ WeatherApp/js/app.js
@@ -5,17 +5,17 @@ $(document).ready(function() {
         location.longitude = getLongitude(data);
         location.regionName = getRegionName(data);
         location.city = getCity(data);
-        $("#city").text(location.city + ", " + location.regionName);
-        var webAddress = "http://api.openweathermap.org/data/2.5/weather?lat=" + location.latitude + "&lon=" + location.longitude + "&APPID=b20df3281dd211ad4c1254577573e180";
+        $("#city").text(location.city);
+        $("#region").text(location.regionName);
+        var webAddress = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=" + location.latitude + "&lon=" + location.longitude + "&cnt=4&APPID=b20df3281dd211ad4c1254577573e180";
         $.getJSON(webAddress, function(data) {
             console.log(data);
-            data.weather[0].id = 300;
-            $("#weather").text(data.weather[0].main);
-            $("#description").text(data.weather[0].description);
-            $("#temperature").text(data.main.temp + " F")
+            $("#weather").text(data.list[0].weather[0].main);
+            //$("#description").text(data.weather[0].description);
+            $("#temperature").text(data.list[0].deg);
+            $("#degree").text('\u00B0' + "F");
             $("#icon").attr("src", getIcon(data));
-        });
-
+        });;
     });
 
     function getLongitude(data) {
@@ -36,34 +36,38 @@ $(document).ready(function() {
 
     function getIcon(data) {
         var path = "";
-        if (200 < data.weather[0].id && data.weather[0].id < 300) {
+        if (200 < data.list[0].weather[0].id && data.list[0].weather[0].id < 300) {
             path = "img/thunder.png";
             return path;
-        } else if (300 <= data.weather[0].id && data.weather[0].id < 400) {
+        } else if (300 <= data.list[0].weather[0].id && data.list[0].weather[0].id < 400) {
             path = "img/drizzle.png";
             return path;
-        } else if (500 <= data.weather[0].id && data.weather[0].id < 600) {
+        } else if (500 <= data.list[0].weather[0].id && data.list[0].weather[0].id < 600) {
             path = "img/rain.png";
             return path;
-        } else if (600 <= data.weather[0].id && data.weather[0].id < 700) {
+        } else if (600 <= data.list[0].weather[0].id && data.list[0].weather[0].id < 700) {
             path = "img/snow.png";
             return path;
-        } else if (700 <= data.weather[0].id && data.weather[0].id < 800) {
+        } else if (700 <= data.list[0].weather[0].id && data.list[0].weather[0].id < 800) {
             path = "img/atmosphere.png";
             return path;
-        } else if (data.weather[0].id == 800) {
+        } else if (data.list[0].weather[0].id == 800) {
             path = "img/clear.png";
             return path;
-        } else if (801 <= data.weather[0].id && data.weather[0].id <= 804) {
+        } else if (801 <= data.list[0].weather[0].id && data.list[0].weather[0].id <= 804) {
             path = "img/cloud.png";
             return path;
-        } else if (900 <= data.weather[0].id && data.weather[0].id <= 906) {
+        } else if (900 <= data.list[0].weather[0].id && data.list[0].weather[0].id <= 906) {
             path = "img/extreme.png";
             return path;
-        } else if (951 <= data.weather[0].id && data.weather[0].id <= 962) {
+        } else if (951 <= data.list[0].weather[0].id && data.list[0].weather[0].id <= 962) {
             path = "img/additional.png";
             return path;
         }
     }
 
+    $( "#degree" ).click(function() {
+      console.log( "Handler for .click() called." );
+      //console.log($("#temperature").text);
+    });
 });

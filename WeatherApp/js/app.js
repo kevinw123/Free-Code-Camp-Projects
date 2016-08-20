da395e6a8ee6113c2d5304c0b60d6f3841e2c475
kevinw123 <kevinwong_895@hotmail.com>
2016-08-19 22:33:24 -0700

    Adding icons and got it to work

--- WeatherApp/js/app.js
+++ WeatherApp/js/app.js
@@ -5,18 +5,17 @@ $(document).ready(function() {
         location.longitude = getLongitude(data);
         location.regionName = getRegionName(data);
         location.city = getCity(data);
+        $("#city").text(location.city + ", " + location.regionName);
         var webAddress = "http://api.openweathermap.org/data/2.5/weather?lat=" + location.latitude + "&lon=" + location.longitude + "&APPID=b20df3281dd211ad4c1254577573e180";
         $.getJSON(webAddress, function(data) {
             console.log(data);
-            var icon = data.weather[0].icon.replace(/"/g,"");
+            data.weather[0].id = 300;
             $("#weather").text(data.weather[0].main);
             $("#description").text(data.weather[0].description);
-            $("#temperature").text(data.main.temp + " F");
-            console.log(icon);
-            //$("icon").attr("src","http://openweathermap.org/img/w/"+icon+".png");
+            $("#temperature").text(data.main.temp + " F")
+            $("#icon").attr("src", getIcon(data));
         });
 
-        $("#city").text(location.city + ", " + location.regionName);
     });
 
     function getLongitude(data) {
@@ -33,6 +32,38 @@ $(document).ready(function() {
 
     function getCity(data) {
         return data.city;
+    }
+
+    function getIcon(data) {
+        var path = "";
+        if (200 < data.weather[0].id && data.weather[0].id < 300) {
+            path = "img/thunder.png";
+            return path;
+        } else if (300 <= data.weather[0].id && data.weather[0].id < 400) {
+            path = "img/drizzle.png";
+            return path;
+        } else if (500 <= data.weather[0].id && data.weather[0].id < 600) {
+            path = "img/rain.png";
+            return path;
+        } else if (600 <= data.weather[0].id && data.weather[0].id < 700) {
+            path = "img/snow.png";
+            return path;
+        } else if (700 <= data.weather[0].id && data.weather[0].id < 800) {
+            path = "img/atmosphere.png";
+            return path;
+        } else if (data.weather[0].id == 800) {
+            path = "img/clear.png";
+            return path;
+        } else if (801 <= data.weather[0].id && data.weather[0].id <= 804) {
+            path = "img/cloud.png";
+            return path;
+        } else if (900 <= data.weather[0].id && data.weather[0].id <= 906) {
+            path = "img/extreme.png";
+            return path;
+        } else if (951 <= data.weather[0].id && data.weather[0].id <= 962) {
+            path = "img/additional.png";
+            return path;
+        }
     }
 
 });

6d1235e6b3ca624f3543def5dba52f9bbbb48574
Kevin Wong <kevinwong_895@hotmail.com>
2016-08-20 17:36:34 -0700

    dropIt.js

---
+++ IntermediateAlgorithm/dropIt.js
@@ -0,0 +1,24 @@
+/*
+ * Drop the elements of an array (first argument), 
+ * starting from the front, until the predicate (second argument) returns true.
+ * The second argument, func, is a function you'll use to test 
+ * the first elements of the array to decide if you should drop it or not.
+ * Return the rest of the array, otherwise return an empty array.
+ */
+function dropElements(arr, func) {
+  // Drop them elements.
+  var result = [];
+  var count = 0;
+  for(var i = 0; i < arr.length; i++){
+    if(!func(arr[i])){
+      count++;
+    }
+    else{
+      break;
+    }
+  }
+  result = arr.slice(i, arr.length);
+  return result;
+}
+
+dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;});
